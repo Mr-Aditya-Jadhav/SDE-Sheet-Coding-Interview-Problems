3d439b6561d417d25263d012432adf7a842e2bcf
Aditya Jadhav <36039636+Mr-Aditya-Jadhav@users.noreply.github.com>
2023-07-10 00:52:34 +0530

    Create Ceil from BST.cpp

---
+++ Day 21/Ceil from BST.cpp
@@ -0,0 +1,51 @@
+//https://www.codingninjas.com/studio/problems/ceil-from-bst_8230754?challengeSlug=striver-sde-challenge
+
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    class BinaryTreeNode {
+    public:
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+        
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+        
+        ~BinaryTreeNode() {
+            if (left) {
+              delete left;
+            }
+            if (right) {
+              delete right;
+            }
+        }
+    };
+
+************************************************************/
+
+int findCeil(BinaryTreeNode<int> *node, int x){
+    // Write your code here.
+    int ans = -1;
+    while(node){
+        if(node->data == x){
+            ans = node->data;
+            return ans;
+        }
+        if(node->data < x){
+            node = node->right;
+        }
+        else{
+            ans = node->data;
+            node = node->left;
+        }
+    }
+
+    return ans;
+
+}
