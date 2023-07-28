1276a6283622bfcc2b7f0883d9d9020b8ede1517
Aditya Jadhav <36039636+Mr-Aditya-Jadhav@users.noreply.github.com>
2023-07-28 22:50:13 +0530

    Create matrix-chain-multiplication.cpp

---
+++ Day 25/matrix-chain-multiplication.cpp
@@ -0,0 +1,65 @@
+//https://practice.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+      int f(int i , int j , int arr[] , vector<vector<int>> &dp ){
+        if(i==j)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int mini = 1e9;
+        for(int k=i ; k<=j-1 ; k++){
+            int steps = arr[i-1]*arr[k]*arr[j] + f(i,k,arr,dp) + f(k+1,j,arr,dp);
+            mini = min(steps, mini);
+        }
+        return dp[i][j]=mini;
+    }
+
+    int matrixMultiplication(int N, int arr[])
+    {
+        // code here
+        vector<vector<int>>dp(N+1,vector<int>(N+1,0));
+        for(int i = 1 ; i< N ; i++){
+            dp[i][i]=0;
+        }
+        for(int i = N-1 ; i>=1 ; i--){
+            for(int j = i+1 ; j<=N-1;j++ ){
+                int mini = 1e9;
+                for(int k=i ; k<=j-1 ; k++){
+                    int steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                    mini = min(steps, mini);
+                }
+                 dp[i][j]=mini;
+            }
+        }
+        return dp[1][N-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.matrixMultiplication(N, arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
